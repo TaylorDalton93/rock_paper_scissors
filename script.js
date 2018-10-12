590eaee7c4d564f684e62d151b3eecf8d4a66da3
T.Dalton <daltontb@miamioh.edu>
2018-10-12 16:14:07 -0400

    Started javascript code

---
+++ script.js
@@ -0,0 +1,11 @@
+var gameTies = 0 , playerWins = 0 , computerWins = 0;
+
+var buttons = document.querySelectorAll("a.button");
+var playerResultBBox = document.getElementById("playerWins");
+var computerResultBox = document.getElementById("computerWins");
+var gameTiesBox = document.getElementById("gameTies");
+var result = document.getElementById("result");
+
+buttons.forEach(function(element) {
+
+}
